b54acbab7615f4a18aec4af61b8de0797d87ee11
sreera02 <srr_developer@protonmail.com>
2024-03-30 17:43:27 -0400

    Added the backend

---
+++ main.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+from flask import Flask, render_template
+import os
+
+TEMPLATES_DIR = os.path.abspath("./templates")
+STATIC_DIR = os.path.abspath("./static")
+app = Flask(__name__)
+app.debug=True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run()
